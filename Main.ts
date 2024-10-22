eafb240950124be408da04c027f0a4c129d8aff6
Luke Dowle <lukedowle88@gmail.com>
2024-10-22 15:51:34 +1300

    Learn for loop

--- Main.ts
+++ Main.ts
@@ -3,3 +3,11 @@ function addNumbers(a: number, b: number) {
 }
 const sum = addNumbers(4, 57);
 console.log(sum);
+
+function loop(num: number, msg: string) {
+  for (let i = 0; i < num; i++) {
+    console.log(msg);
+  }
+}
+
+loop(2, "Hello!")
